d035d6cd89be366f9239a64735aee2a0ba952843
goutham794 <goutham794@gmail.com>
2024-02-08 18:40:56 +0000

    unsloth+nb

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Step 1: Create a virtual environment named 'env'
+python3 -m venv venv
+
+# Step 2: Activate the environment
+source venv/bin/activate
+
+# Step 3: Install packages from requirements.txt
+pip install -r requirements.txt
+
+# pip install "unsloth[cu121_torch220] @ git+https://github.com/unslothai/unsloth.git"
